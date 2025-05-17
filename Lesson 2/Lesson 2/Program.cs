bd8e9e26de3bea1d8a5bb3ff5f4422b1920e7953
FaridSultanov <sultanovvfaridd@gmail.com>
2025-05-17 14:02:47 +0400

    Ikinci dersin tapsiriqlari elave olundu

---
+++ Lesson 2/Lesson 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //string f_n = "";
+            //string a;
+            //int count = 3;
+            //Console.WriteLine("Enter 3 symbols");
+            //while(count>0)
+            //{
+            //    a = Console.ReadLine();
+            //    f_n += a;
+            //    count--;
+            //}
+            //string reverse = new string(f_n.Reverse().ToArray());
+            //Console.WriteLine(reverse);
+            #endregion
+
+            #region Task2
+            //Console.Write("Enter your name:");
+            //string name, surname;
+            //name = Console.ReadLine();
+            //Console.Write("Enter your surname:");
+            //surname = Console.ReadLine();
+            //Console.WriteLine(name.ToUpper()+" "+surname.ToUpper());
+
+            #endregion
+
+            #region Task3
+            //double radius,field,perimetr;
+            //Console.Write("Enter radius:");
+            //radius = Convert.ToDouble(Console.ReadLine());
+            //field = Math.PI * Math.Pow(radius, 2);
+            //perimetr = 2 * radius * Math.PI;
+            //Console.WriteLine("Sahe: "+field);
+            //Console.WriteLine("Perimetr: "+perimetr);
+            #endregion
+
+
+        }
+    }
+}
